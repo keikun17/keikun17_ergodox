c6d269faefcacf89e2bf7c386e45d295c487c65a
Buddy Magsipoc <keikun17@gmail.com>
2016-01-25 23:00:30 +0800

    Swap RALT and RESC。

--- keymaps/keymap_osx_keikun17.c
+++ keymaps/keymap_osx_keikun17.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |Ctrl+A|  Esc |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | '"   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | LAlt | LGui |       | RAlt  |Ctrl/Esc|
+ *                                        | LAlt | LGui |       | Esc  | RAlt |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(1),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_LGUI,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_RALT,        CTL_T(KC_ESC),
+                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_QUOT,
+             KC_ESC, KC_RALT
              KC_PGUP,
              KC_PGDN,KC_TAB, KC_ENT
     ),
